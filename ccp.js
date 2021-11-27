c25421332f85b8aa22fa535b6c1e42275f6914aa
aru99 <mohammadarmanmavric@gmail.com>
2021-11-27 14:53:32 +0530

    revision in progress

--- ccp.js
+++ ccp.js
@@ -1,10 +1,52 @@
 "use strict";
 
-//constant value
-const age = 29;
-//dynamic vaue
-let canDrink = false;
-//tertinary if else statement
-age > 18 ? (canDrink = true) : (canDrink = false);
+// //constant value
+// const age = 29;
+// //dynamic vaue
+// let canDrink = false;
+// //tertinary if else statement, conditional terniary operator
+// age > 18 ? (canDrink = true) : (canDrink = false);
 
-console.log(canDrink);
+// console.log(canDrink);
+// //template literal
+// console.log(`can the person drink : ${canDrink}`);
+
+// //type coercion example
+// let n = "1" + 1;
+// n = n - 1;
+// console.log(n);
+
+//  todo: coding challenge 1
+// const markHeight = 1.69;
+// const markWeight = 78;
+
+// const johnHeight = 1.95;
+// const johnWeight = 92;
+
+// //calculating BMIs
+// const markBmi = markWeight / markHeight ** 2;
+// const johnBmi = johnWeight / johnHeight ** 2;
+
+// let markHeigherBMI = markBmi > johnBmi;
+// console.log(markHeigherBMI);
+// //logic: using conditional tertinary conditional statement to compare the BMI's
+// markBmi > johnBmi
+//   ? console.log(`mark's BMI ${markBmi} is higher than John's BMI ${johnBmi}`)
+//   : console.log(`John's BMI
+// ${johnBmi} is higher than Mark's BMI ${markBmi}`);
+// done:
+
+//todo: CC4
+// const billValue = 250;
+
+// const tipValue =
+//   billValue > 50 && billValue < 300 ? 0.15 * billValue : 0.2 * billValue;
+
+// const totalBillValue = billValue + tipValue;
+
+// console.log(
+//   `The bill was ${billValue} and the Tip was ${tipValue} thus the Total is ${totalBillValue}`
+// );
+// //done:
+
+//todo: Functions coding Challenge
