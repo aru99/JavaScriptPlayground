3e34f91333c5b07771b43bd9a5efb4dfa6a0d7e7
aru99 <mohammadarmanmavric@gmail.com>
2021-12-07 06:20:06 +0530

    this

--- thisPra.js
+++ thisPra.js
@@ -14,3 +14,23 @@ const calcAgeArrow = (birthYear) => {
 };
 
 calcAgeArrow(1998);
+
+const user = {
+  name: "user_name",
+  year: 1998,
+  calcAge: function () {
+    console.log(this);
+    console.log(2021 - this.year);
+  },
+};
+
+user.calcAge();
+
+const user2 = {
+  year: 1996,
+};
+
+//method borrowing, since functions are variables we can do this
+user2.calcAge = user.calcAge;
+
+user2.calcAge();
