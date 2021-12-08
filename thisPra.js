63728f2ce45786fca279ac70ff50e5d5a8467772
aru99 <mohammadarmanmavric@gmail.com>
2021-12-08 13:44:42 +0530

    010

--- thisPra.js
+++ thisPra.js
@@ -1,36 +1,69 @@
 "use strict";
 
-console.log(this);
-const calcAge = function (birthYear) {
-  console.log(2021 - birthYear);
-  console.log(this);
-};
+// console.log(this);
+// const calcAge = function (birthYear) {
+//   console.log(2021 - birthYear);
+//   console.log(this);
+// };
 
-calcAge(1998);
+// calcAge(1998);
 
-const calcAgeArrow = (birthYear) => {
-  console.log(2021 - birthYear);
-  console.log(this);
-};
+// const calcAgeArrow = (birthYear) => {
+//   console.log(2021 - birthYear);
+//   console.log(this);
+// };
 
-calcAgeArrow(1998);
+// calcAgeArrow(1998);
 
+// const user = {
+//   name: "user_name",
+//   year: 1998,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2021 - this.year);
+//   },
+// };
+
+// user.calcAge();
+
+// const user2 = {
+//   year: 1996,
+// };
+
+// //method borrowing, since functions are variables we can do this
+// user2.calcAge = user.calcAge;
+
+// user2.calcAge();
+
+// var name = "matilda";
 const user = {
   name: "user_name",
   year: 1998,
   calcAge: function () {
     console.log(this);
     console.log(2021 - this.year);
+
+    const isBoomer = () => {
+      console.log(this);
+      console.log(this.year >= 1961 && this.year <= 1996);
+    };
+    isBoomer();
+  },
+
+  greet: function () {
+    console.log(`hi ${this.name}`);
   },
 };
 
+user.greet();
 user.calcAge();
 
-const user2 = {
-  year: 1996,
+const addExpress = function () {
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return sum;
 };
 
-//method borrowing, since functions are variables we can do this
-user2.calcAge = user.calcAge;
-
-user2.calcAge();
+console.log(addExpress(2, 3, 5));
