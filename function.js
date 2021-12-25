7bdf26141dd4aaf8c4d902fb1995203c3e035a4c
aru99 <mohammadarmanmavric@gmail.com>
2021-12-25 22:04:44 +0530

    Default parameters done

---
+++ function.js
@@ -0,0 +1,23 @@
+"use strict";
+/*
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassangers = 1,
+  price = 199 * numPassangers
+) {
+  //ES5 way of having deffault values
+  //numPassangers = numPassangers || 1;
+  //price = price || 199;
+  const booking = {
+    flightNum,
+    numPassangers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking("LH123", 10);
+*/
