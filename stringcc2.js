b6f57714a8beb706be9cc65d1c2ab661e215c850
aru99 <mohammadarmanmavric@gmail.com>
2021-12-25 11:11:17 +0530

    1070

--- stringcc2.js
+++ stringcc2.js
@@ -7,31 +7,31 @@ const flights =
 //   🔴 Delayed Arrival from HEL to FAO (12h05)
 //            Departure from FAO to LIS (12h30)
 
+//todo: Refactoring the below code base to DRY
 console.log(flights.split("+ "));
+
+//fn1
+const trimStr = (str) => str.toLowerCase().replaceAll("_", " ").trim();
+//fn2
+const trimStr2 = (str) => str.replaceAll("_", " ").trim();
 
 for (const plane of flights.split("+")) {
   const [type, from, to, time] = plane.split(";");
   //   console.log(type.toLowerCase().replaceAll("_", " ").trim());
   if (
-    type.toLowerCase().replaceAll("_", " ").trim() === `delayed departure` ||
-    type.toLowerCase().replaceAll("_", " ").trim() === `delayed arrival`
+    trimStr(type) === `delayed departure` ||
+    trimStr(type) === `delayed arrival`
   ) {
     console.log(
-      `🔴${type.replaceAll("_", " ").trim()} ${from
+      `🔴${trimStr2(type)} ${from.slice(0, 3).toUpperCase()} to ${to
         .slice(0, 3)
-        .toUpperCase()} to ${to.slice(0, 3).toUpperCase()} ${time.replaceAll(
-        ":",
-        "hr"
-      )}`.padStart(40)
+        .toUpperCase()} ${time.replaceAll(":", "hr")}`.padStart(40)
     );
   } else {
     console.log(
-      `${type.replaceAll("_", " ").trim()} ${from
+      `${trimStr2(type)} ${from.slice(0, 3).toUpperCase()} to ${to
         .slice(0, 3)
-        .toUpperCase()} to ${to.slice(0, 3).toUpperCase()} ${time.replace(
-        ":",
-        "hr"
-      )}`.padStart(40)
+        .toUpperCase()} ${time.replace(":", "hr")}`.padStart(40)
     );
   }
 }
