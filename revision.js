1d46853eacd0a8575604bb28e0610372f7b9d8eb
aru99 <mohammadarmanmavric@gmail.com>
2022-02-10 19:28:16 +0530

    minag

--- revision.js
+++ revision.js
@@ -1,1 +1,28 @@
 "use strict";
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  //function to order food
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+};
+
+//destructuring array, destructuring assignment ']='
+const arr = [1, 2, 3, 4];
+const [a, b, c, d] = arr;
+console.log(a, b, c, d);
+
+//way of swapping variables without using a thirs variable temp
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+//ordering food
+let [starter, maincourse] = restaurant.order(2, 2);
+console.log(starter, "&", maincourse);
