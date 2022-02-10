985d516858e26110261999adb4c36d3b3ec662a4
aru99 <mohammadarmanmavric@gmail.com>
2022-02-10 19:35:34 +0530

    minag

--- revision.js
+++ revision.js
@@ -26,3 +26,12 @@ console.log(main, secondary);
 //ordering food
 let [starter, maincourse] = restaurant.order(2, 2);
 console.log(starter, "&", maincourse);
+
+//nested destructuring
+const nested = [2, 3, [4, 5]];
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+//default values, when getting data from API
+const [p = 1, q = 1, r = 1] = [4, 5];
+console.log(p, q, r);
