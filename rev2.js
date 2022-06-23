6c711709b60c5d6e69cdd581f910d9f6dabbb433
aru99 <mohammadarmanmavric@gmail.com>
2022-06-23 18:32:14 +0530

    22622

--- rev2.js
+++ rev2.js
@@ -4,7 +4,7 @@ let mark_height, mark_mass, mark_bmi, john_height, john_mass, john_bmi;
 let BMI = (user_height, user_mass) => {
   return user_mass / user_height ** 2;
 };
-let markHigherBMI = true;
+//let markHigherBMI = true;
 mark_height = 1.88;
 mark_mass = 95;
 mark_bmi = BMI(mark_height, mark_mass);
@@ -13,9 +13,14 @@ john_height = 1.76;
 john_mass = 85;
 john_bmi = BMI(john_height, john_mass);
 
-if (mark_bmi > john_bmi) {
-  console.log(markHigherBMI);
-} else {
-  markHigherBMI = false;
-  console.log(markHigherBMI);
-}
+// this was a very basic way of comparison
+// if (mark_bmi > john_bmi) {
+//   console.log(markHigherBMI);
+// } else {
+//   markHigherBMI = false;
+//   console.log(markHigherBMI);
+// }
+
+// better way of comparision
+const markHigherBMI = mark_bmi > john_bmi;
+console.log(markHigherBMI);
