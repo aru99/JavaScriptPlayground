184c218d0ab005f766ab70dda5c73aefa6968b86
aru99 <mohammadarmanmavric@gmail.com>
2022-10-20 21:39:08 +0530

    revising

--- revision/script.js
+++ revision/script.js
@@ -1,4 +1,16 @@
 "use strict";
 
-var name = "first name";
-console.log(name);
+// Mark info : height and weight
+const markHeight = 169;
+const markWeight = 78;
+
+// John info : height and weight
+const johnHeight = 195;
+const johnWeight = 92;
+
+const markHeigherBmi =
+  markWeight / markHeight ** 2 > johnWeight / johnHeight ** 2;
+console.log(markHeigherBmi);
+
+// var name = "first name";
+// console.log(name);
