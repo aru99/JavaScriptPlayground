21274a122e99b411d9910b459e50200570977206
aru99 <mohammadarmanmavric@gmail.com>
2022-10-23 21:51:45 +0530

    done for the day WIAG

--- revision/game_revision/script.js
+++ revision/game_revision/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+document.querySelector('.message').textContent = `correct number 🎉`;
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
