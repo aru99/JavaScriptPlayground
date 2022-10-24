2904fbb99d25e1fea956be099cd947c5f54e1625
aru99 <mohammadarmanmavric@gmail.com>
2022-10-24 16:20:12 +0530

    Implemented : DRY

--- revision/game_revision/script.js
+++ revision/game_revision/script.js
@@ -11,6 +11,11 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
+
+// functions
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 //------------------EventListener for Playing the game------------------
 document.querySelector('.check').addEventListener('click', function () {
   //converting the retrieved user input to number
@@ -19,11 +24,13 @@ document.querySelector('.check').addEventListener('click', function () {
   //   checking if there is any input or not
   //logic: if there is no input, we will get 0 as a response, now 0 is a falsy value, but we want something to happen when there is 0 as a response, thus we negate the falsy value to convert into truthy value
   if (!guess) {
-    document.querySelector('.message').textContent = `⛔ No number entered !`;
+    // document.querySelector('.message').textContent = `⛔ No number entered !`;
+    displayMessage(`⛔ No number entered !`);
   }
   //   When player wins
   else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = `🎉 Correct Number`;
+    // document.querySelector('.message').textContent = `🎉 Correct Number`;
+    displayMessage(`🎉 Correct Number`);
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b437';
     document.querySelector('.number').style.width = '30rem';
@@ -34,28 +41,42 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.highscore').textContent = highScore;
     }
   }
-  //   when guess is too high
-  else if (guess > secretNumber) {
+
+  //   when guess wrong
+  else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = `📈 Too High`;
+      //   document.querySelector('.message').textContent =
+      //     guess > secretNumber ? `📈 Too High` : `📉 Too low`;
+      displayMessage(guess > secretNumber ? `📈 Too High` : `📉 Too low`);
       // reducing the score value
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = `😵 Game Over`;
+      //   document.querySelector('.message').textContent = `😵 Game Over`;
+      displayMessage(`😵 Game Over`);
     }
   }
-  //   when guess is too low
-  else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = `📉 Too low`;
-      // reducing the score value
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = `😵 Game Over`;
-    }
-  }
+  //   } else if (guess > secretNumber) {
+  //     if (score > 1) {
+  //       document.querySelector('.message').textContent = `📈 Too High`;
+  //       // reducing the score value
+  //       score--;
+  //       document.querySelector('.score').textContent = score;
+  //     } else {
+  //       document.querySelector('.message').textContent = `😵 Game Over`;
+  //     }
+  //   }
+  //   //   when guess is too low
+  //   else if (guess < secretNumber) {
+  //     if (score > 1) {
+  //       document.querySelector('.message').textContent = `📉 Too low`;
+  //       // reducing the score value
+  //       score--;
+  //       document.querySelector('.score').textContent = score;
+  //     } else {
+  //       document.querySelector('.message').textContent = `😵 Game Over`;
+  //     }
+  //   }
 });
 
 //------------------Eventlistener for Resetting the game------------------
@@ -69,7 +90,8 @@ document.querySelector('.again').addEventListener('click', function () {
   // remove value from the input field
   document.querySelector('.guess').value = '';
   // reset message
-  document.querySelector('.message').textContent = `Start guessing ...`;
+  //   document.querySelector('.message').textContent = `Start guessing ...`;
+  displayMessage(`Start guessing ...`);
 
   // reset score
   score = 20;
