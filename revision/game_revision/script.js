89bbd09ac4c65a25cc7e5f76c0e009f00b19c3a3
aru99 <mohammadarmanmavric@gmail.com>
2022-10-24 13:07:20 +0530

    implemented score reduction

--- revision/game_revision/script.js
+++ revision/game_revision/script.js
@@ -7,6 +7,11 @@
 
 // document.querySelector('.guess').value = 23;
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   //converting the retrieved user input to number
   const guess = Number(document.querySelector('.guess').value);
@@ -15,6 +20,17 @@ document.querySelector('.check').addEventListener('click', function () {
   //logic: if there is no input, we will get 0 as a response, now 0 is a falsy value, but we want something to happen when there is 0 as a response, thus we negate the falsy value to convert into truthy value
   if (!guess) {
     document.querySelector('.message').textContent = `⛔ No number entered !`;
-  } else {
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = `🎉 Correct Number`;
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = `📈 Too High`;
+    // reducing the score value
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = `📉 Too low`;
+    // reducing the score value
+    score--;
+    document.querySelector('.score').textContent = score;
   }
 });
