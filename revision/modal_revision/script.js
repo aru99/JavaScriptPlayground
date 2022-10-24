b7eec7d22c0be6e63cb6742499d39bf8b6936f3a
aru99 <mohammadarmanmavric@gmail.com>
2022-10-24 19:29:03 +0530

    closed modal

--- revision/modal_revision/script.js
+++ revision/modal_revision/script.js
@@ -4,3 +4,16 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log(`button clicked`);
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+btnCloseModal.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
