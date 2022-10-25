52524d0d673012e6a5926f46c0290b83b2989c94
aru99 <mohammadarmanmavric@gmail.com>
2022-10-25 08:08:47 +0530

    dynamic current score, active player toggle : implemented

--- revision/dice_game_rev/script.js
+++ revision/dice_game_rev/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const current0El = document.querySelector('#current--0');
@@ -12,7 +14,9 @@ const btnRoll = document.querySelector('.btn--roll');
 const benHold = document.querySelector('.btn--hold');
 
 //
-let currentScore;
+const score = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 // starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -29,9 +33,15 @@ btnRoll.addEventListener('click', function () {
   // -> chsck for rolled 1, if true, switch player
   if (dice != 1) {
     currentScore += dice;
-    // change later
-    current0El.textContent = currentScore;
+    //  selecting active player dynamically
+    document.querySelector(`#current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // switch to next player
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
