95ca9f20851a7b0fd3dac0a24108398c2eafcde2
aru99 <mohammadarmanmavric@gmail.com>
2022-10-25 07:51:33 +0530

    current score implemented

--- revision/dice_game_rev/script.js
+++ revision/dice_game_rev/script.js
@@ -3,10 +3,16 @@
 // selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const benHold = document.querySelector('.btn--hold');
+
+//
+let currentScore;
 // starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -21,4 +27,11 @@ btnRoll.addEventListener('click', function () {
   diceEl.src = `dice-${dice}.png`;
 
   // -> chsck for rolled 1, if true, switch player
+  if (dice != 1) {
+    currentScore += dice;
+    // change later
+    current0El.textContent = currentScore;
+  } else {
+    // switch to next player
+  }
 });
