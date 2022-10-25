851bdacb04befe85b402353d515d97b8acc36683
aru99 <mohammadarmanmavric@gmail.com>
2022-10-25 14:07:36 +0530

    hold score implemented

--- revision/dice_game_rev/script.js
+++ revision/dice_game_rev/script.js
@@ -11,16 +11,26 @@ const current1El = document.querySelector('#current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
-const benHold = document.querySelector('.btn--hold');
+const btnHold = document.querySelector('.btn--hold');
 
 //
-const score = [0, 0];
+const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 // starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+// player switch function
+
+const switchPlayer = function () {
+  document.querySelector(`#current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 // rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -38,10 +48,18 @@ btnRoll.addEventListener('click', function () {
       currentScore;
   } else {
     // switch to next player
-    document.querySelector(`#current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    currentScore = 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    switchPlayer();
   }
+});
+
+btnHold.addEventListener('click', function () {
+  // -> add current score to the acitve player's score
+  //   scores[1] = scores[1]  currentScore
+  scores[activePlayer] += currentScore;
+  document.querySelector(`#score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  //   ->check if player score is above 100, if so finish game
+  //    switch to the next player
+  switchPlayer();
 });
