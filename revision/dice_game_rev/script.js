fed3b70d75e6bfbc0abdce88eb5d4cc4fa73c76f
aru99 <mohammadarmanmavric@gmail.com>
2022-10-25 15:54:05 +0530

    finished game

--- revision/dice_game_rev/script.js
+++ revision/dice_game_rev/script.js
@@ -14,15 +14,27 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 //
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
-// starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
 
+// starting conditions
+let scores, currentScore, activePlayer, playing;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+init();
 // player switch function
 
 const switchPlayer = function () {
@@ -64,7 +76,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     //   ->check if player score is above 100, if so finish game
-    if (scores[activePlayer] >= 30) {
+    if (scores[activePlayer] >= 10) {
       //finish the game
       diceEl.classList.add('hidden');
       playing = false;
@@ -81,3 +93,7 @@ btnHold.addEventListener('click', function () {
     //    switch to the next player
   }
 });
+
+//resetting the game
+
+btnNew.addEventListener('click', init);
