9ad4724427ee3f88369707a0190def1417de2dc1
aru99 <mohammadarmanmavric@gmail.com>
2022-10-25 07:38:03 +0530

    random dice roll implemented

--- revision/dice_game_rev/script.js
+++ revision/dice_game_rev/script.js
@@ -4,8 +4,21 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const diceEl = document.querySelector('.dice');
-
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const benHold = document.querySelector('.btn--hold');
 // starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+// rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // ->generate random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  //   ->display Dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // -> chsck for rolled 1, if true, switch player
+});
