8a9285bc75e24b315e31533d0dfb0f1c4b4ed721
aru99 <mohammadarmanmavric@gmail.com>
2022-10-27 20:57:28 +0530

    timings added

--- revision/script2.js
+++ revision/script2.js
@@ -7,13 +7,34 @@ const restaurant = {
   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
 
+//   opening hours
+openingHours : {
+thu : {
+    open:12,
+    close 22,
+},
+fri : {
+    open:11,
+    close 23,
+},
+sat : {
+    open:0, // Open 24hrs
+    close 24,
+},
+},
+
   //   function to order food
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 };
 
-//
+//Destructuring objects
+
+
+
+/** 
+//Destructuring Arrays
 let [main, , secondary] = restaurant.categories;
 console.log(main, secondary);
 // switching the variables
@@ -25,3 +46,4 @@ const [starterOrder, mainCourseOrder] = restaurant.order(2, 0);
 const order = `The customer has ordered : 
 ${starterOrder} and ${mainCourseOrder}`;
 console.log(order);
+*/
