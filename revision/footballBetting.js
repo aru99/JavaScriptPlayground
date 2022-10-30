dd9c6e86b7941c10b0bcec4dc28db242bf7f2f25
aru99 <mohammadarmanmavric@gmail.com>
2022-10-30 18:54:02 +0530

    5 done

--- revision/footballBetting.js
+++ revision/footballBetting.js
@@ -51,8 +51,17 @@ const [gk, ...fieldPlayers] = players1;
 const allPlayers = [...game.players];
 // players plus substitute
 const player1Final = ["Thiago", "Coutinho", "Perisic", ...players1];
-console.log(`Players 1 : ${players1} 
+// creating varaibles from object properties
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(
+  `Players 1 : ${players1} 
 Players2 : ${players2}
 Goal Keeper and rest players : ${gk} and ${fieldPlayers}
 All players : ${allPlayers}
-Players1Final : ${player1Final}`);
+Players1Final : ${player1Final}`,
+  team1,
+  draw,
+  team2
+);
