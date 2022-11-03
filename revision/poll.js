a67388f6764b7b9c06cef784dff19b386f4bdc4e
aru99 <mohammadarmanmavric@gmail.com>
2022-11-03 17:17:23 +0530

    questions and answers object done

--- revision/poll.js
+++ revision/poll.js
@@ -1,1 +1,8 @@
 "use strict";
+
+//poll object
+const poll = {
+  question: "what is your favourite programming language ?",
+  options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+  answers: new Array(4).fill(0),
+};
