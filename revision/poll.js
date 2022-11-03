d8da017aac47a9d1f79e56110bf6405a5c8cff7a
aru99 <mohammadarmanmavric@gmail.com>
2022-11-03 17:42:06 +0530

    checking answer number, type and limit

--- revision/poll.js
+++ revision/poll.js
@@ -5,4 +5,21 @@ const poll = {
   question: "what is your favourite programming language ?",
   options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
   answers: new Array(4).fill(0),
+  //   get answer
+  registerNewAnswer() {
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join("\n")}\n(write option number)`
+      )
+    );
+    console.log(answer);
+    // register Answer
+    typeof answer === "number" &&
+      answer >= 0 &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+    console.log(this.answers);
+  },
 };
+
+poll.registerNewAnswer();
