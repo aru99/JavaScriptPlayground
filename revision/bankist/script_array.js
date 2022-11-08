ce1b74703e51b470f192eece003083e0a176788f
aru99 <mohammadarmanmavric@gmail.com>
2022-11-08 21:08:45 +0530

    created a function for looping over and displaying the transactions

--- revision/bankist/script_array.js
+++ revision/bankist/script_array.js
@@ -61,6 +61,24 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
+const display_movements = function (movements) {
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? "deposit" : `withdrawal`;
+    const html = `
+    <div class="movements__row">
+    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+     
+    <div class="movements__value">${mov}</div>
+  </div>
+  `;
+    //   <div class="movements__date">3 days ago</div>
+    // adding the new html element
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+  });
+};
+
+display_movements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -71,10 +89,49 @@ const currencies = new Map([
   ["GBP", "Pound sterling"],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+let closingBalance = 0;
 
-const arr = ["a", "b", "c", "d", "e", "f"];
+// looping over the array using for of
+for (const movement of movements) {
+  if (movement > 0) {
+    closingBalance += movement;
+    console.log(`You deposited ${movement}, current balance ${closingBalance}`);
+  } else if (movement < 0) {
+    closingBalance -= movement;
+    console.log(
+      `You withdrew ${Math.abs(movement)}, current balance ${closingBalance}`
+    );
+  }
+}
 
-console.log(arr.slice(2, 4));
+console.log(`------FOR EACH------------`);
+
+movements.forEach(function (movement, index, arr) {
+  if (movement > 0) {
+    closingBalance += movement;
+    console.log(
+      ` transaction no : ${
+        index + 1
+      } of ::You deposited ${movement}, current balance ${closingBalance}`
+    );
+  } else if (movement < 0) {
+    closingBalance -= movement;
+    console.log(
+      `transaction no : ${index + 1} :: You withdrew ${Math.abs(
+        movement
+      )}, current balance ${closingBalance}`
+    );
+  }
+});
+
+// const currencies = new Map([
+//   ["USD", "United States dollar"],
+//   ["EUR", "Euro"],
+//   ["GBP", "Pound sterling"],
+// ]);
+
+currencies.forEach(function (value, key, map) {});
