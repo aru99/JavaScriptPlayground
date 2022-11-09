aead6203f597c283e05bdb48bb55ac16766f2b12
aru99 <mohammadarmanmavric@gmail.com>
2022-11-09 16:06:03 +0530

    completed CC1

--- revision/bankist/cc1.js
+++ revision/bankist/cc1.js
@@ -4,4 +4,22 @@
 const juliaArr = [3, 5, 2, 12, 7];
 const kateArr = [4, 1, 15, 8, 3];
 
-const checkDogs = function (dogsjulia, dogskate) {};
+const checkDogs = function (dogsjulia, dogskate) {
+  // creating a shallow copy
+  const dogsJuliaCorrected = dogsjulia.slice();
+  dogsJuliaCorrected.splice(0, 1);
+  dogsJuliaCorrected.splice(-2);
+  console.log(dogsJuliaCorrected);
+  const dogs = dogsJuliaCorrected.concat(dogskate);
+  console.log(dogs);
+  //looping over dogs and finding which is dog which is puppy
+  dogs.forEach(function (dog, i) {
+    if (dog >= 3) {
+      console.log(`Dog number ${i + 1} is an Adult and is ${dog} years old`);
+    } else if (dog < 3) {
+      console.log(`DOg number ${i + 1} is stil a Puppy`);
+    }
+  });
+};
+
+checkDogs(juliaArr, kateArr);
