2fd282ba0bdc71635873feb7fe84edd8d4343b24
aru99 <mohammadarmanmavric@gmail.com>
2022-11-12 12:11:39 +0530

    implemented loan functionality

--- revision/bankist/script_array.js
+++ revision/bankist/script_array.js
@@ -216,6 +216,30 @@ btnTransfer.addEventListener("click", function (e) {
   }
 });
 
+// ------------ Loan implementation-----------------
+
+// ------------ Delete Account implementation-----------------
+// inputCloseUsername = document.querySelector(".form__input--user");
+// const inputClosePin
+btnClose.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(function (acc) {
+      return acc.username === currentAccount.username;
+    });
+
+    // delete account
+    accounts.splice(index, 1);
+
+    // hide UI
+    containerApp.style.opacity = 0;
+  }
+
+  inputCloseUsername.value = inputClosePin.value = "";
+});
 //
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
