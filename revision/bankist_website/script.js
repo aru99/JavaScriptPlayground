55594182df5ad3b6d52de4e3dc5ba2ad87bf33bc
aru99 <mohammadarmanmavric@gmail.com>
2022-11-15 12:57:33 +0530

    added cookie message

--- revision/bankist_website/script.js
+++ revision/bankist_website/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,10 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +32,27 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+/////////////////////////////////////////////////////////////////////////
+
+const header = document.querySelector('header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+// creating and inserting elements
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML =
+  'we use cookies for improved functionality and analytics. <button class ="btn btn--close-cookie">Got it!</button>';
+
+// header.prepend(message);
+header.append(message);
+//Deleting elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
