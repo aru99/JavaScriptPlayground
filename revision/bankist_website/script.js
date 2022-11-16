abd18c86a7877c5f482c5f51f8dfb6244370fbd2
aru99 <mohammadarmanmavric@gmail.com>
2022-11-16 21:37:35 +0530

    added tab switching using event delegation

--- revision/bankist_website/script.js
+++ revision/bankist_website/script.js
@@ -81,6 +81,33 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
+// Tabbed component----------------------
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // guard clause
+  if (!clicked) {
+    return;
+  }
+
+  // remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // active tab
+  clicked.classList.add('operations__tab--active');
+  // active content
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+  // console.log(clicked);
+  // console.log(clicked.dataset);
+});
+
 /////////////////////////////////////////////////////////////////////////
 
 /*
