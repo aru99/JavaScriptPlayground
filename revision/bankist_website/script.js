80018ec6eb490abd460033000ce93fd93829211d
aru99 <mohammadarmanmavric@gmail.com>
2022-11-17 12:51:19 +0530

    hover fade effect on nav added

--- revision/bankist_website/script.js
+++ revision/bankist_website/script.js
@@ -6,6 +6,10 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
 ///////////////////////////////////////
 // -----------------------Modal window------------------------------------
 
@@ -82,9 +86,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 });
 
 // Tabbed component----------------------
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
 
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
@@ -106,6 +107,39 @@ tabsContainer.addEventListener('click', function (e) {
     .classList.add('operations__content--active');
   // console.log(clicked);
   // console.log(clicked.dataset);
+});
+
+//-----menu fade effect----
+nav.addEventListener('mouseover', function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    //
+    siblings.forEach(el => {
+      if (el !== link) {
+        el.style.opacity = 0.5;
+      }
+    });
+    logo.style.opacity = 0.5;
+  }
+});
+
+nav.addEventListener('mouseout', function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    //
+    siblings.forEach(el => {
+      if (el !== link) {
+        el.style.opacity = 1;
+      }
+    });
+    logo.style.opacity = 1;
+  }
 });
 
 /////////////////////////////////////////////////////////////////////////
