2e5418a204e1dd0956b625ac74d26bbc977d6342
aru99 <mohammadarmanmavric@gmail.com>
2022-11-17 21:39:40 +0530

    implemented reveal sections on scroll using inersection observer API

--- revision/bankist_website/script.js
+++ revision/bankist_website/script.js
@@ -134,15 +134,18 @@ const handelHover = function (e) {
 
 const header = document.querySelector('.header');
 const navHeight = nav.getBoundingClientRect().height;
+//callback function for intersectionObserver
 const stickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entry);
+  // console.log(entry);
   if (!entry.isIntersecting) {
     nav.classList.add('sticky');
   } else {
     nav.classList.remove('sticky');
   }
 };
+
+// IntersectionObserver API
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
@@ -150,14 +153,31 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 headerObserver.observe(header);
 
-// const initialCoords = section1.getBoundingClientRect();
-// window.addEventListener('scroll', function () {
-//   if (window.scrollY > initialCoords.top) {
-//     nav.classList.add('sticky');
-//   } else {
-//     nav.classList.remove('sticky ');
-//   }
-// });
+// ----- reveal sections-----
+
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) {
+    return;
+  } else {
+    entry.target.classList.remove('section--hidden');
+  }
+  observer.unobserve(entry.target);
+};
+
+// Intersection observer API
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
 
 /////////////////////////////////////////////////////////////////////////
 
