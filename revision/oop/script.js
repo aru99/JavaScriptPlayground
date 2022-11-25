84ba9dade42d6f4e7da16a7556866c86c0e04d87
aru99 <mohammadarmanmavric@gmail.com>
2022-11-25 12:33:04 +0530

    oop

--- revision/oop/script.js
+++ revision/oop/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const arman = new Person('Arman', 1998);
+console.log(arman);
