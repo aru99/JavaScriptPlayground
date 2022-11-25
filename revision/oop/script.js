e03981d0b2f8028be101c9853e11ac56064f0b90
aru99 <mohammadarmanmavric@gmail.com>
2022-11-25 21:13:03 +0530

    getters and setters wiag

--- revision/oop/script.js
+++ revision/oop/script.js
@@ -24,7 +24,6 @@ ashima.calcAge();
 Person.prototype.species = 'homo sapiens';
 console.log(arman, ashima);
 console.log(arman.__proto__.__proto__);
-*/
 
 //classes
 
@@ -36,16 +35,36 @@ console.log(arman.__proto__.__proto__);
 class personCl {
   //this has to be constructor, issa rule
   constructor(firstName, birthYear) {
-    this.firstName = firstName;
-    this.birthYear = birthYear;
-  }
-  //what ever function we write would be on the prototype property of the class
-  calcAge() {
-    console.log(2022 - this.birthYear);
-  }
+      this.firstName = firstName;
+      this.birthYear = birthYear;
+    }
+    //what ever function we write would be on the prototype property of the class
+    calcAge() {
+        console.log(2022 - this.birthYear);
+    }
 }
 
 const nora = new personCl('Nora', 1998);
 const jessi = new personCl('jessi', 1996);
 jessi.calcAge();
 nora.calcAge();
+*/
+
+// getters and setters
+
+const account = {
+  owner: 'Jonas',
+  movements: [200, 500, 120, 300, 450],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
