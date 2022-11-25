388f14bd02b8fd7456d96d32cde1b9b5b79d7e39
aru99 <mohammadarmanmavric@gmail.com>
2022-11-25 20:16:31 +0530

    classes wiag

--- revision/oop/script.js
+++ revision/oop/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
@@ -23,3 +24,28 @@ ashima.calcAge();
 Person.prototype.species = 'homo sapiens';
 console.log(arman, ashima);
 console.log(arman.__proto__.__proto__);
+*/
+
+//classes
+
+// class expression
+//const personCl = class{}
+
+//class declaration
+
+class personCl {
+  //this has to be constructor, issa rule
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+  //what ever function we write would be on the prototype property of the class
+  calcAge() {
+    console.log(2022 - this.birthYear);
+  }
+}
+
+const nora = new personCl('Nora', 1998);
+const jessi = new personCl('jessi', 1996);
+jessi.calcAge();
+nora.calcAge();
