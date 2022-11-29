9f0c9b115b5b1e437c77f7689880159035380d52
aru99 <mohammadarmanmavric@gmail.com>
2022-11-29 21:22:33 +0530

    testing cc1

--- revision/async/script.js
+++ revision/async/script.js
@@ -87,7 +87,7 @@ const renderCountry = function (data, className) {
               `;
 
   countriesContainer.insertAdjacentHTML('beforeend', html);
-  //   countriesContainer.style.opacity = 1;
+  countriesContainer.style.opacity = 1;
 };
 // promises
 const request = fetch(`https://restcountries.com/v2/name/portugal`);
@@ -105,29 +105,92 @@ console.log(request);
 //     });
 // };
 
-const getCountryData = function (country) {
-  fetch(`https://restcountries.com/v2/name/${country}`)
-    .then(response => response.json())
-    .then(data => {
-      renderCountry(data[0]);
-      const neighbour = data[0].borders?.[0];
-      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
-    })
-    .then(response => response.json())
-    .then(data => renderCountry(data, 'neighbour'))
-    .catch(err => {
-      renderError(`Something went wrong XD ${err.message} TRY again `);
-    })
-    .finally(() => {
-      countriesContainer.style.opacity = 1;
-    });
+const getJson = function (url, errMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(`${errMsg}, ${response.status} `);
+    }
+    return response.json();
+  });
 };
 
-const renderError = function (msg) {
-  countriesContainer.insertAdjacentText('beforeend', msg);
-};
-btn.addEventListener('click', function () {
-  getCountryData('portugal');
-});
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v2/name/${country}`)
+//     .then(response => {
+//       console.log(response);
+//       // manually creating error message
+//       if (!response.ok) {
+//         throw new Error();
+//       }
+//       return response.json();
+//     })
+//     .then(data => {
+//       renderCountry(data[0]);
+//       const neighbour = data[0].borders?.[0];
+//       return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+//     })
+//     .then(response => response.json())
+//     .then(data => renderCountry(data, 'neighbour'))
+//     .catch(err => {
+//       renderError(`Something went wrong XD ${err.message} TRY again `);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
 
-getCountryData('ssdsd');
+// const getCountryData = function (country) {
+//   getJson(`https://restcountries.com/v2/name/${country}`, `country not found`)
+//     .then(data => {
+//       renderCountry(data[0]);
+//       const neighbour = data[0].borders?.[0];
+//       if (!neighbour) throw new Error('No neighbour found !');
+//       return getJson(
+//         `https://restcountries.com/v2/alpha/${neighbour}`,
+//         `Country not found`
+//       );
+//     })
+//     .then(data => renderCountry(data, 'neighbour'))
+//     .catch(err => {
+//       renderError(`Something went wrong XD ${err.message} TRY again `);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
+
+// const renderError = function (msg) {
+//   countriesContainer.insertAdjacentText('beforeend', msg);
+// };
+// btn.addEventListener('click', function () {
+//   getCountryData('australia');
+// });
+
+// getCountryData('ssdsd');
+
+// function
+// const whereAmI = function (lat, lng) {
+//   //fetching
+//   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+//     .then(res => {
+//       if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+//       return res.json();
+//     })
+//     .then(data => {
+//       console.log(data);
+//       console.log(`You are in ${data.city}, ${data.country}`);
+//       return fetch(`https://restcountries.com/v2/name/${data.country}`);
+//     })
+//     .then(res => {
+//       if (!res.ok) throw new Error(`Country not found(${res.status})`);
+//       return res.json();
+//     })
+//     .then(data => renderCountry(data[0]))
+//     .catch(err => console.error(`${err.message} 💣`));
+// };
+
+// whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+// whereAmI(-33.933, 18.474);
+
+console.log();
