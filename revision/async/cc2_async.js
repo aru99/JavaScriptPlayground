5ef046d95b4a125d6342e09050a92f90531b12ad
aru99 <mohammadarmanmavric@gmail.com>
2022-11-30 23:14:45 +0530

    cc3part 1 done

--- revision/async/cc2_async.js
+++ revision/async/cc2_async.js
@@ -25,24 +25,47 @@ const createImage = function (imgPath) {
   });
 };
 
-let currentImage;
-createImage('img/img-1.jpg')
-  .then(img => {
-    currentImage = img;
-    console.log(`image 1 loaded`);
-    return wait(2);
-  })
-  .then(() => {
-    currentImage.style.display = 'none';
-    return createImage('img/img-2.jpg');
-  })
-  .then(img => {
-    currentImage = img;
-    console.log(`img 2 loaded`);
-    return wait(2);
-  })
-  .then(() => {
-    currentImage.style.display = 'none';
-  })
-  .catch(err => console.error(err));
-// cc3
+// let currentImage;
+// createImage('img/img-1.jpg')
+//   .then(img => {
+//     currentImage = img;
+//     console.log(`image 1 loaded`);
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImage.style.display = 'none';
+//     return createImage('img/img-2.jpg');
+//   })
+//   .then(img => {
+//     currentImage = img;
+//     console.log(`img 2 loaded`);
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImage.style.display = 'none';
+//   })
+//   .catch(err => console.error(err));
+
+//----- cc3-----
+const loadNpause = async function () {
+  try {
+    // Load img 1
+    let img = await createImage('img/img-1.jpg');
+    console.log(`Image 1 loaded`);
+    await wait(2);
+    img.style.display = 'none';
+
+    // img 2
+    // Load img 1
+    img = await createImage('img/img-2.jpg');
+    console.log(`Image 1 loaded`);
+    await wait(2);
+    img.style.display = 'none';
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+loadNpause();
+
+//
