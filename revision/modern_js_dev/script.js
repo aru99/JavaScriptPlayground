49f11f5e77b7e5086e3ebd4d91381901e0ae1af7
aru99 <mohammadarmanmavric@gmail.com>
2022-12-02 11:40:08 +0530

    promises

--- revision/modern_js_dev/script.js
+++ revision/modern_js_dev/script.js
@@ -1,6 +1,12 @@
-// Importing Module
-import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// // Importing Module
+// import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 
-console.log('Importing module');
-addToCart('eggs', 10);
-console.log(price, tq);
+// console.log('Importing module');
+// addToCart('eggs', 10);
+// console.log(price, tq);
+
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+if (module.hot) {
+  module.hot.accept();
+}
